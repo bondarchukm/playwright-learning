da4ddb2c0f60662655017f77bb890465fb714c3f
bondarchukm <bondarchuk.hh@gmail.com>
2021-05-31 19:49:41 +0300

    jest test runner setup

--- spec/login-test.spec.ts
+++ spec/login-test.spec.ts
@@ -1,51 +1,38 @@
-import {
-    it,
-    describe,
-    expect,
-    beforeAll,
-    afterAll,
-    beforeEach,
-    afterEach,
-} from '@playwright/test'
+import { expect } from '@playwright/test'
 import { LoginPage } from '../pages/login.page'
 import { userData } from '../lib/userData'
 import { Browser, BrowserContext, chromium, Page } from 'playwright'
 import { ProductsPage } from '../pages/products.page'
 import { productsPageTitle } from '../lib/pageTitles'
-import { loginPageURL } from '../lib/urls'
 
 let browser: Browser
-let page: Page
 let context: BrowserContext
+let page: Page
 
-describe('Login tests', async () => {
-    // beforeAll(async () => {
-    //     browser = await chromium.launch()
-    // })
-    // beforeEach(async () => {
-    //     context = await browser.newContext()
-    //     page = await context.newPage()
-    // })
-    // afterAll(async () => {
-    //     await browser.close()
-    // })
-    // afterEach(async () => {
-    //     await page.close()
-    // })
-
-    it('Should login successfully with valid credentials', async ({
-        context,
-    }) => {
-        const page = await context.newPage()
-
-        const loginPage = new LoginPage(page)
-        const productsPage = new ProductsPage(page)
-
-        await loginPage.navigateTo()
-        await loginPage.setUserName(userData.standardUser)
-        await loginPage.setPassword(userData.password)
-        await loginPage.clickLoginButton()
-
-        expect(await productsPage.getPageTitleText()).toEqual(productsPageTitle)
+beforeAll(async () => {
+    browser = await chromium.launch({
+        headless: true,
     })
+})
+beforeEach(async () => {
+    context = await browser.newContext()
+    page = await context.newPage()
+})
+afterAll(async () => {
+    await browser.close()
+})
+afterEach(async () => {
+    await page.close()
+})
+
+test('Should login successfully with valid credentials', async () => {
+    const loginPage = new LoginPage(page)
+    const productsPage = new ProductsPage(page)
+
+    await loginPage.navigateTo()
+    await loginPage.setUserName(userData.standardUser)
+    await loginPage.setPassword(userData.password)
+    await loginPage.clickLoginButton()
+
+    expect(await productsPage.getPageTitleText()).toEqual(productsPageTitle)
 })
